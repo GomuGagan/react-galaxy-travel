80c0f777bc80722e070eccd8aa11c70e90f1254f
GomuGagan <gagangomu@gmail.com>
2022-10-07 16:23:34 +0530

    hero image done, work on pricing section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
+import Pricing from './pages/Pricing';
 
 const App = () => {
   return (
     <div>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/pricing" element={<Pricing />} />
 
         </Routes>
     </div>
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import HeroImage from '../components/HeroImage'
+import Navbar from '../components/Navbar'
+
+const Pricing = () => {
+  return (
+    <div>
+        <Navbar />
+        <HeroImage heading="Pricing" text="Choose your Trip."/>
+        <Footer />
+    </div>
+  )
+}
+
+export default Pricing
